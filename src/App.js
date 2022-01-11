ae4f4b5723c565a30360a3d15b26bf6b33ffddcb
David <david_sova@ukr.net>
2022-01-11 21:16:27 +0200

    Props through route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
+let dialogs = [
+  { id: "1", name: "Dimych" },
+  { id: "2", name: "Andrew" },
+  { id: "3", name: "Sveta" },
+  { id: "4", name: "Sasha" },
+  { id: "5", name: "Viktor" },
+  { id: "6", name: "Valera" },
+];
+
+let messages = [
+  { id: "1", message: "Hi" },
+  { id: "2", message: "How are you?" },
+  { id: "3", message: "Yo" },
+  { id: "4", message: "Yo" },
+  { id: "5", message: "Yo" },
+];
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,8 +31,11 @@ const App = () => {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path='/dialogs/*' element={<Dialogs/>} />
-            <Route path='/profile' element={<Profile/>} />
+            <Route
+              path="/dialogs/*"
+              element={<Dialogs dialogs={dialogs} messages={messages} />}
+            />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </div>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,27 +3,13 @@ import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
-let dialogs = [
-  {id: '1', name: 'Dimych'},
-  {id: '2', name: 'Andrew'},
-  {id: '3', name: 'Sveta'},
-  {id: '4', name: 'Sasha'},
-  {id: '5', name: 'Viktor'},
-  {id: '6', name: 'Valera'}
-];
 
-let messages = [
-  {id: '1', message: 'Hi'},
-  {id: '2', message: 'How are you?'},
-  {id: '3', message: 'Yo'},
-  {id: '4', message: 'Yo'},
-  {id: '5', message: 'Yo'}
-];
+
 
 const Dialogs = (props) => {
-
-  let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-  let messagesElements = messages.map(m => <Message message={m.message} />);
+  console.log(props);
+  let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} key={d.id} />);
+  let messagesElements = props.messages.map(m => <Message message={m.message} key={m.id}/>);
 
   return (
     <div className={s.dialogs}>
