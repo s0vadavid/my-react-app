a71bd320c6554fac988a163e9ac15cb52e6a2b97
David <david_sova@ukr.net>
2022-08-10 17:36:04 +0300

    Reducers

---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,26 @@
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
+const SEND_MESSAGE = "SEND-MESSAGE";
+
+const dialogsReducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY:
+      state.newMessageBody = action.body;
+      return state;
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      state.newMessageBody = "";
+      state.messages.push({ id: "6", message: body });
+      return state;
+    default:
+      return state;
+  }
+};
+
+export const updateNewMessageBodyCreator = (body) => {
+  return { type: UPDATE_NEW_MESSAGE_BODY, body: body };
+};
+export const sendMessageCreator = () => {
+  return { type: SEND_MESSAGE };
+};
+
+export default dialogsReducer;
--- src/redux/stateInit.js
+++ src/redux/stateInit.js
@@ -1,7 +1,6 @@
-const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
-const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
-const SEND_MESSAGE = "SEND-MESSAGE";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
 
 let store = {
   _callSubscriber() {
@@ -36,7 +35,7 @@ let store = {
         { id: "6", name: "Valera" },
       ],
 
-      newMessageBody: '',
+      newMessageBody: "",
     },
 
     sidebar: {},
@@ -51,42 +50,12 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === ADD_POST) {
-      let newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-      };
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      this._state.profilePage.newPostText = action.newText;
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.dialogsPage.newMessageBody = action.body;
-      this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      let body = this._state.dialogsPage.newMessageBody;
-      this._state.dialogsPage.newMessageBody = '';
-      this._state.dialogsPage.messages.push({ id: '6', message: body })
-      this._callSubscriber(this._state);
-    }
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+    this._callSubscriber(this._state);
   },
 };
-
-export const addPostActionCreator = () => {
-  return { type: ADD_POST };
-}
-export const updateNewPostTextActionCreator = (text) => {
-  return { type: UPDATE_NEW_POST_TEXT, newText: text }
-}
-export const updateNewMessageBodyCreator = (body) => {
-  return { type: UPDATE_NEW_MESSAGE_BODY, body: body }
-}
-export const sendMessageCreator = () => {
-  return { type: SEND_MESSAGE };
-}
 
 window.store = store;
 export default store;
